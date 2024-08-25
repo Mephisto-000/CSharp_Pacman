dbe6acb2a238fb8326c35df39d559391e8a87b24
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-08-25 22:47:57 +0800

    feat: update Game, Ghosts, Pacman classes

--- Pacman/ConsolePacman/Ghosts.cs
+++ Pacman/ConsolePacman/Ghosts.cs
@@ -1,7 +1,30 @@
-﻿public class Ghosts
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+public class Ghosts
 {
-    public Ghosts(int ghostCount)
+    public List<Ghost> Members { get; set; }
+    public World World { get; set; }
+    public static ConsoleColor[] Colors =
+        [ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.Magenta];
+
+
+    public Ghosts(World world, int ghostCount)
     {
-        // TODO: Ghosts
+        World = world;
+        Members = new List<Ghost>();
+        for (int i = 0; i < ghostCount; i++)
+        {
+            Members.Add(new Ghost(World, i));
+        }
     }
+}
+
+public class Ghost(World world, int index)
+{
+    public int index;
+    public Position Position = world.GhostStartPositions[index];
+    public Direction Direction = Direction.None;
+    public ConsoleColor Color = Ghosts.Colors[index];
 }
--- Pacman/ConsolePacman/Pacman.cs
+++ Pacman/ConsolePacman/Pacman.cs
@@ -1,6 +1,13 @@
 ﻿public class Pacman
 {
-    public Pacman()
+    private readonly World _world;
+    public Position Position { get; set; }
+    public Direction Direction { get; set; }
+
+    public Pacman(World world)
     {
+        _world = world;
+        Position = _world.PacmanStartPosition;
+        Direction = Direction.None;
     }
 }
